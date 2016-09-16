16d732ba8d301a81b139eb0e5c38c8beb6f4702a
fp12 <fab.nospam+github@gmail.com>
2016-09-16 01:15:51 -0400

    Actions bar is now made of Hex Magic as well

--- src/ui/actions_bar.py
+++ src/ui/actions_bar.py
@@ -1,55 +1,80 @@
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.uix.scatterlayout import ScatterLayout
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.properties import StringProperty
+
+from ui.hex_widget import HexWidget
 
 from game import game_instance
 from actions import ActionType
+from hex_lib import Hex, Layout
 
 
-class UnitActionButton(Button):
-    def __init__(self, index, action_type, rk_skill=None, **kwargs):
+class UnitActionTile(ButtonBehavior, HexWidget):
+    action = StringProperty()
+
+    def __init__(self, index, action_type, text, rk_skill=None, **kwargs):
         self.index = index
         self.action_type = action_type
         self.rk_skill = rk_skill
-        super(UnitActionButton, self).__init__(**kwargs)
+        self.action = text
+        super(UnitActionTile, self).__init__(**kwargs)
 
 
-class ActionsBar(BoxLayout):
+class ActionsBar(ScatterLayout):
+    __Layouts__ = [[Hex(0, 0)],
+                   [Hex(0, 0), Hex(1, -1)],
+                   [Hex(-1, 0), Hex(0, 0), Hex(1, -1)],
+                   [Hex(-1, 0), Hex(0, 0), Hex(1, -1), Hex(0, -1)],
+                   [Hex(-1, 0), Hex(0, 0), Hex(1, -1), Hex(0, -1), Hex(2, -2)],
+                   [Hex(-1, 0), Hex(-1, 1), Hex(0, 1), Hex(1, 0), Hex(1, -1), Hex(0, 0)],
+                   [Hex(-1, 1), Hex(0, 1), Hex(1, 0), Hex(-1, 0), Hex(0, 0), Hex(1, -1), Hex(0, -1)]]
+
+    def __init__(self, **kwargs):
+        super(ActionsBar, self).__init__(**kwargs)
+        self.hex_layout = Layout(origin=self.center, size=35, flat=True, margin=3)
+
     def create(self):
         game_instance.current_fight.on_next_turn += lambda unit: self.on_new_action(unit, None, unit.actions_tree)
         game_instance.current_fight.on_action_change += self.on_new_action
 
-    def create_action_widget(self, index, action_type, text, rk_skill=None):
-        new_widget = UnitActionButton(index=index, action_type=action_type, text=text, rk_skill=rk_skill)
-        new_widget.bind(on_press=self.on_button_pressed)
+    def create_action_widget(self, hex_coords, index, action_type, text, rk_skill=None):
+        new_widget = UnitActionTile(index, action_type, text, rk_skill, q=hex_coords.q, r=hex_coords.r, layout=self.hex_layout)
         self.add_widget(new_widget)
 
     def on_new_action(self, unit, action_type, action_node, _rk_skill=None):
         self.clear_widgets()
+        widget_data = []
         index = 1
         for a in action_node:
             if a.data in [ActionType.Weapon, ActionType.Skill]:
                 for rk_skill in unit.get_skills(a.data):
-                    self.create_action_widget(index=index, action_type=a.data, text=rk_skill.skill.name, rk_skill=rk_skill)
+                    widget_data.append((index, a.data, rk_skill.skill.name, rk_skill))
                     index += 1
-
-            elif a.data == ActionType.EndTurn:
-                self.create_action_widget(index=0, action_type=a.data, text='End Turn')
-
-            else:
-                self.create_action_widget(index=index, action_type=a.data, text=a.data.name)
+            elif a.data != ActionType.EndTurn:
+                widget_data.append((index, a.data, a.data.name, None))
                 index += 1
+        count = len(widget_data) # not including the mandatory End Turn !
+        assert(count < len(ActionsBar.__Layouts__))
+        for i, w_data in enumerate(widget_data):
+            index, action_type, text, rk_skill = w_data
+            self.create_action_widget(ActionsBar.__Layouts__[count][i], index, action_type, text, rk_skill)
+        self.create_action_widget(ActionsBar.__Layouts__[count][count], 0, ActionType.EndTurn, 'End Turn')
 
-    def _on_action_selected(self, index, button=None):
+    def _on_action_selected(self, index=None, button=None):
         if not button:
-            for ch in self.children:
-                if ch.index == index:
-                    button = ch
+            for child in self.content.children:
+                if child.index == index:
+                    button = child
                     break
         if button:
             game_instance.current_fight.notify_action_change(button.action_type, button.rk_skill)
 
     def on_key_pressed(self, code, key):
-        self._on_action_selected(int(key))
+        self._on_action_selected(index=int(key))
 
-    def on_button_pressed(self, button):
-        self._on_action_selected(button.index)
+    def on_touch_down(self, touch):
+        hover_hex = self.hex_layout.pixel_to_hex(touch.pos).get_round()
+        for child in self.content.children:
+            if child.hex_coords == hover_hex:
+                self._on_action_selected(button=child)
+                return True
