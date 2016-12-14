aaf6a7f47e2979f629965aa7b920e486a3ed242c
Cody <cody@quantifiedcode.com>
2016-12-14 21:56:40 +0000

    Migrated `%` string formating

--- src/localization/core.py
+++ src/localization/core.py
@@ -21,7 +21,7 @@ def init():
   locale.setlocale(locale.LC_ALL, '')  # use user's preferred locale
   loc = locale.getlocale()
   loc_code = loc[0][0:2]  # take first two characters of country code
-  filename = "data/loc/yahtr_%s.mo" % loc_code
+  filename = "data/loc/yahtr_{0!s}.mo".format(loc_code)
 
   try:
     trans = gettext.GNUTranslations(open(filename, "rb"))
